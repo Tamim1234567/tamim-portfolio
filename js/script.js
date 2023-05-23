81704182b3acec277984a2a9a92d50918c54feb4
Tamim1234567 <tmir1309@gmail.com>
2023-05-23 01:23:26 -0400

    making it more responsive

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ var typed = new Typed("#element", {
   ],
   typeSpeed: 120,
 });
+function toggleShow() {
+  document.getElementById("show-nav").style.display = "none";
+  document.getElementById("logo").style.display = "none";
+  document.getElementById("hide-nav").style.display = "block";
+  document.getElementById("hide-on-sm").style.display = "flex";
+  document.querySelector("nav").style.flexDirection = "column";
+}
+function toggleHide() {
+  document.getElementById("show-nav").style.display = "block";
+  document.getElementById("logo").style.display = "block";
+  document.getElementById("hide-nav").style.display = "none";
+  document.getElementById("hide-on-sm").style.display = "none";
+  document.querySelector("nav").style.flexDirection = "row";
+}
